8f22b1dc87ae509208e174bcb102c5ccf93bd33d
SingularitySurfer <norman_krackow@gmx.de>
2024-11-24 20:19:09 +0100

    switch to RTIC

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,46 @@
 #![no_std]
 #![cfg_attr(not(doc), no_main)]
 
-use panic_rtt_target as _;
-use rtt_target::{rprintln, rtt_init_print};
+#[rtic::app(device = stm32f1xx_hal::pac)]
+mod app {
+    use panic_rtt_target as _;
+    use rtt_target::{rprintln, rtt_init_print};
+    use cortex_m_rt::entry;
+    use nb::block;
+    use stm32f1xx_hal::{
+        gpio::{gpioa::PA0, gpioc::PC13, Edge, ExtiPin, Input, Output, PullDown, PushPull},
+        prelude::*,
+    };
+    use stm32f1xx_hal::{pac, prelude::*, timer::Timer};
 
-use cortex_m_rt::entry;
-use nb::block;
+    #[shared]
+    struct Shared {}
 
-use stm32f1xx_hal::{pac, prelude::*, timer::Timer};
+    #[local]
+    struct Local {}
 
-#[entry]
-fn main() -> ! {
-    rtt_init_print!();
-    rprintln!("Hello, Rust!");
+    #[init]
+    fn init(mut ctx: init::Context) -> (Shared, Local, init::Monotonics) {
+        rtt_init_print!();
+        rprintln!("Hello, Rust!");
 
-    let cp = cortex_m::Peripherals::take().unwrap();
-    let dp = pac::Peripherals::take().unwrap();
+        let mut flash = ctx.device.FLASH.constrain();
+        let rcc = ctx.device.RCC.constrain();
 
+        let clocks = rcc.cfgr.freeze(&mut flash.acr);
 
-    let mut flash = dp.FLASH.constrain();
-    let rcc = dp.RCC.constrain();
+        let mut timer = Timer::syst(ctx.core.SYST, &clocks).counter_hz();
+        timer.start(1.Hz()).unwrap();
 
-    let clocks = rcc.cfgr.freeze(&mut flash.acr);
-
-    let mut timer = Timer::syst(cp.SYST, &clocks).counter_hz();
-    timer.start(1.Hz()).unwrap();
-
-    let mut i = 0;
-    loop {
-        block!(timer.wait()).unwrap();
-        i += 1;
-        rprintln!("Hello again; I have blinked {} times.", i);
-        if i == 10 {
-            panic!("Yow, 10 times is enough!");
+        let mut i = 0;
+        loop {
+            block!(timer.wait()).unwrap();
+            i += 1;
+            rprintln!("Hello again; I have blinked {} times.", i);
+            if i == 10 {
+                panic!("Yow, 10 times is enough!");
+            }
         }
+        (Shared {}, Local {}, init::Monotonics())
     }
 }
